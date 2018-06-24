fe08b6ad81a699c8897ac400477aadcbe14c1308
Andy Teucher <andy.teucher@gmail.com>
2018-06-24 16:04:40 +0000

    version 0.2.0

--- R/tz_lookup.R
+++ R/tz_lookup.R
@@ -1,8 +1,19 @@
-#' Lookup time zones of points
+#' Lookup time zones of sf or sp points
+#'
+#' There are two methods - `"fast"`, and `"accurate"`. The `"fast"` version can
+#' look up many thousands of points very quickly, however  when a point is near
+#' a time zone boundary and not near a populated centre, it may return the
+#' incorrect timezone. If accuracy is more important than speed, use
+#' `method = "accurate"`.
 #'
 #' @param x either an `sfc` or `sf` points or `SpatialPoints(DataFrame)` object
 #' @param crs the coordinate reference system: integer with the EPSG code, or character with proj4string.
 #' If not specified (i.e., `NULL`) and `x` has no existing `crs`, EPSG: 4326 is assumed (lat/long).
+#' @param method method by which to do the lookup. Either `"fast"` (default)
+#' or `"accurate"`.
+#' @param warn By default, if `method = "fast"` a warning is issued about
+#'   the potential for inaccurate results. Set `warn` to `FALSE` to turn
+#'   this off.
 #'
 #' @return character vector the same length as `x` specifying the time zone of the points.
 #' @export
@@ -21,78 +32,65 @@
 #' plot(state_centers_sf[, "tz"])
 #' }
 #'
-tz_lookup <- function(x, crs = NULL) {
-  UseMethod("tz_lookup")
+tz_lookup <- function(x, crs = NULL, method = "fast", warn = TRUE) {
+  switch(method,
+         fast = tz_lookup_fast(x, crs, warn),
+         accurate = tz_lookup_accurate(x, crs),
+         stop("method mst be one of 'fast' or 'accurate'", call. = FALSE))
+
 }
 
-#' @export
-tz_lookup.sf <- function(x, crs = NULL) {
-  if (!requireNamespace("sf"))
-    stop("You must have the sf package installed to use this function", call. = FALSE)
+tz_lookup_fast <- function(x, crs = NULL, warn) {
+  UseMethod("tz_lookup_fast")
+}
 
-  if (!all(sf::st_geometry_type(x) == "POINT"))
-    stop("This only works with points", call. = FALSE)
-
-  if (is.na(sf::st_crs(x))) {
-    if (is.null(crs)) crs <- 4326
-    sf::st_crs(x) <- crs
-  }
-
-  transform <- sf::st_crs(x) != sf::st_crs(4326)
-  if (transform) {
-    x <- sf::st_transform(x, crs = 4326)
-  }
-
+tz_lookup_fast.sf <- function(x, crs, warn) {
+  x <- fix_sf(x, crs)
   coords <- sf::st_coordinates(x)
   # sf stores as x, y and tzlookup likes lat, lon (Which is the opposite)
-  tz_lookup_coords(lat = coords[, 2], lon = coords[, 1])
+  tz_lookup_coords_fast(lat = coords[, 2], lon = coords[, 1], warn = warn)
 }
 
-#' @export
-tz_lookup.sfc <- tz_lookup.sf
+tz_lookup_fast.sfc <- tz_lookup_fast.sf
 
-#' @export
-tz_lookup.SpatialPoints <- function(x, crs = NULL) {
-  if (!requireNamespace("sp"))
-    stop("You must have the sp package installed to use this function", call. = FALSE)
-
-  if (is.numeric(crs)) crs <- paste0("+init=epsg:", crs)
-
-  if (is.na(sp::proj4string(x))) {
-    if (is.null(crs)) crs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-    sp::proj4string(x) <- sp::CRS(crs)
-  }
-
-  if (!is_wgs84(x)) {
-    x <- sp::spTransform(x, sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-  }
+tz_lookup_fast.SpatialPoints <- function(x, crs, warn) {
+  x <- fix_sp(x, crs)
 
   coords <- sp::coordinates(x)
   # sf stores as x, y and tzlookup likes lat, lon (Which is the opposite)
-  tz_lookup_coords(lat = coords[, 2], lon = coords[, 1])
+  tz_lookup_coords_fast(lat = coords[, 2], lon = coords[, 1], warn = warn)
 }
 
-#' Lookup time zones of points
+#' Lookup time zones of lat/long pairs
+#'
+#' There are two methods - `"fast"`, and `"accurate"`. The `"fast"` version can
+#' look up many thousands of points very quickly, however  when a point is near
+#' a time zone boundary and not near a populated centre, it may return the
+#' incorrect timezone. If accuracy is more important than speed, use
+#' `method = "accurate"`.
 #'
 #' @param lat numeric vector of latitudes
 #' @param lon numeric vector of longitudes the same length as `x`
-#'
+#' @inheritParams tz_lookup
 #' @return character vector the same length as x and y specifying the time zone of the points.
 #' @export
 #'
 #' @examples
 #' tz_lookup_coords(42, -123)
 #' tz_lookup_coords(lat = c(48.9, 38.5, 63.1, -25), lon = c(-123.5, -110.2, -95.0, 130))
-tz_lookup_coords <- function(lat, lon) {
-  if (inherits(lat, c("sf", "sfc", "SpatialPoints"))) {
-    stop("It looks like you are trying to get the tz of an sf/sfc or SpatialPoints object! Use tz_lookup() instead.",
-         call. = FALSE)
-  }
+tz_lookup_coords <- function(lat, lon, method = "fast", warn = TRUE) {
+  check_for_spatial(lat)
+  check_coords(lat, lon)
 
-  if (!identical(length(lat), length(lon)) || !all(is.numeric(lat) && is.numeric(lon))) {
-    stop("lat and lon must numeric vectors be of the same length")
-  }
+  switch(method,
+    fast = tz_lookup_coords_fast(lat, lon, warn),
+    accurate = tz_lookup_coords_accurate(lat, lon),
+    stop("method mst be one of 'fast' or 'accurate'", call. = FALSE)
+  )
+}
 
+tz_lookup_coords_fast <- function(lat, lon, warn) {
+  if (warn) warn_for_fast()
   ctx <- make_ctx()
 
   ctx$assign("lat", lat)
@@ -103,9 +101,44 @@ tz_lookup_coords <- function(lat, lon) {
   ret
 }
 
-is_wgs84 <- function(x) {
-  # The most minimal specification of WGS84
-  comp <- c("+proj=longlat", "+datum=WGS84", "+no_defs")
-  x_str <- strsplit(sp::CRS(sp::proj4string(x))@projargs, " +")[[1]]
-  all(comp %in% x_str)
+tz_lookup_accurate <- function(x, crs = NULL) {
+  UseMethod("tz_lookup_accurate")
+}
+
+tz_lookup_accurate.sf <- function(x, crs = NULL) {
+  x <- fix_sf(x, crs)
+  x <- suppressMessages(sf::st_join(x, tz_sf))
+  ret <- x$tzid
+
+  # If any are NA, try to fill in with V8-based tzlookup
+  nas <- which(is.na(ret))
+  if (!length(nas)) {
+    return(ret)
+  }
+  ret[nas] <- tz_lookup_fast(x[nas, ], warn = FALSE)
+  ret
+}
+
+tz_lookup_accurate.sfc <- function(x, crs = NULL) {
+  x_sf <- sf::st_sf(id = seq_len(length(x)), geom = x)
+  tz_lookup_accurate(x_sf, crs = crs)
+}
+
+tz_lookup_accurate.SpatialPoints <- function(x, crs = NULL) {
+  x <- sf::st_as_sf(x)
+  tz_lookup_accurate(x, crs)
+}
+
+tz_lookup_coords_accurate <- function(lat, lon) {
+
+  ll <- data.frame(lat = lat, lon = lon)
+  # check for NAs before converting to sf, only send valid lat/lon pairs
+  # into sf, recombine with NAs for output
+  cc <- stats::complete.cases(ll)
+  ret <- rep(NA_character_, length(cc))
+  if (sum(cc)) {
+    ll_sf <- sf::st_as_sf(ll[cc, ], coords = c("lon", "lat"), crs = 4326)
+    ret[cc] <- tz_lookup_accurate(ll_sf)
+  }
+  ret
 }
--- README.md
+++ README.md
@@ -1,32 +1,56 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
-[![Travis-CI Build Status](https://travis-ci.org/ateucher/lutz.svg?branch=master)](https://travis-ci.org/ateucher/lutz) [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/ateucher/lutz?branch=master&svg=true)](https://ci.appveyor.com/project/ateucher/lutz) [![Coverage Status](https://img.shields.io/codecov/c/github/ateucher/lutz/master.svg)](https://codecov.io/github/ateucher/lutz?branch=master)
 
-lutz (look up timezones)
-========================
+[![Travis-CI Build
+Status](https://travis-ci.org/ateucher/lutz.svg?branch=master)](https://travis-ci.org/ateucher/lutz)
+[![AppVeyor Build
+Status](https://ci.appveyor.com/api/projects/status/github/ateucher/lutz?branch=master&svg=true)](https://ci.appveyor.com/project/ateucher/lutz)
+[![Coverage
+Status](https://img.shields.io/codecov/c/github/ateucher/lutz/master.svg)](https://codecov.io/github/ateucher/lutz?branch=master)
 
-Input latitude and longitude values or an `sf` or `sfc` POINT object and get back the timezone in which they exist. This package uses the **V8** package to access the [`tz-lookup.js` javascript library](https://github.com/darkskyapp/tz-lookup/).
+# lutz (look up timezones)
 
-Installation
-------------
+Input latitude and longitude values or an `sf/sfc` POINT object and get
+back the timezone in which they exist. Two methods are implemented. One
+is very fast and uses the *V8* package to access the [`tz-lookup.js`
+javascript library](https://github.com/darkskyapp/tz-lookup/). However,
+speed comes at the cost of accuracy - near time zone borders away from
+populated centres there is a chance that it will return the incorrect
+time zone.
 
-You can install lutz from github with:
+The other method is slower but more accurate - it uses the sf package to
+intersect points with a detailed map of time zones from
+[here](https://github.com/evansiroky/timezone-boundary-builder).
+
+## Installation
+
+You can install lutz from CRAN with:
+
+``` r
+install.packages("lutz")
+```
+
+Or you can install the development version from github with:
 
 ``` r
 # install.packages("devtools")
 devtools::install_github("ateucher/lutz")
 ```
 
-Example
--------
+## Examples
 
-There are only two functions in this package: `tz_lookup()` which works with both `sf/sfc` and `SpatialPoints` objects, and `tz_lookup_coords` for looking up lat/long pairs.
+There are only two functions in this package: `tz_lookup()` which works
+with both `sf/sfc` and `SpatialPoints` objects, and `tz_lookup_coords`
+for looking up lat/long pairs. Use the `method` argument to choose the
+`"fast"` or `"accurate"` method.
 
 ### With coordinates. They must be lat/long in decimal degrees:
 
 ``` r
 library(lutz)
-tz_lookup_coords(49.5, -123.5)
+tz_lookup_coords(49.5, -123.5, method = "fast")
+#> [1] "America/Vancouver"
+tz_lookup_coords(49.5, -123.5, method = "accurate")
 #> [1] "America/Vancouver"
 
 tz_lookup_coords(lat = c(48.9, 38.5, 63.1, -25), lon = c(-123.5, -110.2, -95.0, 130))
@@ -48,6 +72,7 @@ state_centers_sf <- st_sf(st_sfc(pts))
 
 # Use tz_lookup_sf to find the timezones
 state_centers_sf$tz <- tz_lookup(state_centers_sf)
+state_centers_sf$tz <- tz_lookup(state_centers_sf, method = "accurate")
 
 ggplot() + 
   geom_sf(data = state_centers_sf, aes(colour = tz)) + 
@@ -55,7 +80,7 @@ ggplot() +
   coord_sf(datum = NA)
 ```
 
-![](tools/readme/unnamed-chunk-3-1.png)
+![](tools/readme/unnamed-chunk-4-1.png)<!-- -->
 
 ### With `SpatialPoints` objects:
 
@@ -72,4 +97,63 @@ ggplot(cbind(as.data.frame(coordinates(state_centers_sp)), tz = state_centers_sp
   theme_minimal()
 ```
 
-![](tools/readme/unnamed-chunk-4-1.png)
+![](tools/readme/unnamed-chunk-5-1.png)<!-- -->
+
+We can compare the accuracy of both methods to the high-resolution
+timezone map provided by
+<https://github.com/evansiroky/timezone-boundary-builder>. This is the
+map that is used by `lutz` for the `"accurate"` method, but in `lutz` it
+is simplified by about 80% to be small enough to fit in the
+package.
+
+``` r
+## Get the full timezone geojson from https://github.com/evansiroky/timezone-boundary-builder
+download.file("https://github.com/evansiroky/timezone-boundary-builder/releases/download/2018d/timezones-with-oceans.geojson.zip",
+                destfile = "tz.zip")
+unzip("tz.zip", exdir = "data-raw/dist/")
+```
+
+``` r
+library(lutz)
+library(sf)
+library(rmapshaper)
+library(purrr)
+
+tz_full <- read_sf("data-raw/dist/combined-with-oceans.json")
+# Create a data frame of 500000 lat/long pairs:
+set.seed(1)
+n <- 500000
+ll <- data.frame(lat = runif(n, -90, 90), lon = runif(n, -180, 180))
+ll_sf <- st_as_sf(ll, coords = c("lon", "lat"), crs = 4326)
+
+# Overlay those points with the full high-resolution timezone map:
+ref_ll_tz <- sf::st_join(ll_sf, tz_full)
+#> although coordinates are longitude/latitude, st_intersects assumes that they are planar
+
+# run tz_lookup with both `"fast"` and `"accurate"` methods and compare with 
+# the timezones looked up with the high-resolution map:
+tests <- map_df(c("fast", "accurate"), ~ {
+  time <- system.time(test_ll_tz <- tz_lookup(ll_sf, method = .x, warn = FALSE))
+  comp <- ref_ll_tz$tzid == test_ll_tz
+  matches <- sum(comp, na.rm = TRUE)
+  mismatches <- sum(!comp, na.rm = TRUE)
+  list(
+    method = .x,
+    time = time["elapsed"],
+    matches = matches,
+    mismatches = mismatches,
+    accuracy = matches / (matches + mismatches),
+    ref_nas = sum(is.na(ref_ll_tz$tzid)),
+    fun_nas = sum(is.na(test_ll_tz))
+    )
+})
+```
+
+``` r
+knitr::kable(tests)
+```
+
+| method   |   time | matches | mismatches | accuracy | ref\_nas | fun\_nas |
+| :------- | -----: | ------: | ---------: | -------: | -------: | -------: |
+| fast     |  2.721 |  384735 |     115265 | 0.769470 |        0 |        0 |
+| accurate | 47.587 |  499956 |         44 | 0.999912 |        0 |        0 |
